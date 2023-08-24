81704cc10224bee6074a184e674e6fd20cf32c3e
seonghyeon.seo <seonghyeon.seo@stonebuild.co.kr>
2023-08-24 11:05:46 +0900

    Added: ch02 done.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # react-project
+
+ch01 리액트 기본
+ch02 github profile
---
+++ ch02/src/App.js
@@ -0,0 +1,24 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Profile from './pages/Profile';
+import Projects from './pages/Projects';
+import ProjectDetail from './pages/ProjectDetail';
+import logo from './assets/logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Header logo={ logo } />
+          <Routes>
+            <Route path='/' element={<Profile userName='tintachina84' />} />
+            <Route path='/projects' element={<Projects userName='tintachina84' />} />
+            <Route path='/projects/:name' element={<ProjectDetail userName='tintachina84' />} />
+          </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
