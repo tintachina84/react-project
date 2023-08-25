bbb49d7f5168898f2c5a466f5250a111faa7601e
seonghyeon.seo <tintachina84@gmail.com>
2023-08-25 13:53:57 +0900

    Added: ch03: styled-component added and ch03 is done.

--- ch03/src/components/Lane/Lane.js
+++ ch03/src/components/Lane/Lane.js
@@ -1,5 +1,25 @@
 import Task from '../Task/Task';
-import './Lane.css';
+import styled from 'styled-components';
+
+const LaneWrapper = styled.div`
+  text-align: left;
+  padding: 0;
+  background: lightGray;
+  border-radius: 20px;
+  min-height: 50vh;
+  width: 20vw;
+
+  @media (max-width: 768px) {
+    margin-bottom: 5%;
+  }
+`;
+
+const Title = styled.h2`
+  width: 100%;
+  padding-bottom: 10px;
+  text-align: center;
+  border-bottom: 1px solid darkGray;
+`;
 
 function Lane({
                 laneId,
@@ -12,11 +32,11 @@ function Lane({
                 onDrop
 }) {
   return (
-    <div className='Lane-wrapper'
+    <LaneWrapper
          onDragOver={onDragOver}
          onDrop={(e) => onDrop(e, laneId)}
     >
-      <h2>{title}</h2>
+      <Title>{title}</Title>
       { loading || error ? (
         <span>{ error || 'Loading...' }</span>
       ) : (
@@ -30,7 +50,7 @@ function Lane({
             />
         ))
       )}
-    </div>
+    </LaneWrapper>
   );
 }
 
