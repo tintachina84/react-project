8414e63bb5206be9da782ab6b2074d4fb387115c
seonghyeon.seo <tintachina84@gmail.com>
2023-08-31 16:47:57 +0900

    Added: ch05 - Using life cycles in functional components.

--- ch05/src/context/ItemsContext.js
+++ ch05/src/context/ItemsContext.js
@@ -1,12 +1,29 @@
-import { createContext } from 'react';
-import useDataFetching from '../hooks/useDataFetching';
+import { createContext, useCallback, useState } from 'react';
 
 export const ItemsContext = createContext();
 
 export const ItemsContextProvider = ({ children }) => {
-    const [loading, error, data] = useDataFetching('https://my-json-server.typicode.com/PacktPublishing/React-Projects-Second-Edition/items',);
+    // const [loading, error, data] = useDataFetching('https://my-json-server.typicode.com/PacktPublishing/React-Projects-Second-Edition/items',);
+    const [loading, setLoading] = useState(true);
+    const [items, setItems] = useState([]);
+    const [error, setError] = useState('');
+
+    const fetchItems = useCallback(async (listId) => {
+        try {
+            const data = await fetch(`https://my-json-server.typicode.com/PacktPublishing/React-Projects-Second-Edition/lists/${listId}/items`);
+            const result = await data.json();
+            if (result) {
+                setItems(result);
+                setLoading(false);
+            }
+        } catch (e) {
+            setLoading(false);
+            setError(e.message);
+        }
+    }, []);
+
     return (
-        <ItemsContext.Provider value={{ items: data, loading, error }}>
+        <ItemsContext.Provider value={{ items, fetchItems, loading, error }}>
             {children}
         </ItemsContext.Provider>
     );
--- ch05/src/pages/ListDetail.js
+++ ch05/src/pages/ListDetail.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useContext } from 'react';
+import { useEffect, useContext } from 'react';
 import styled from 'styled-components';
 import { useNavigate, useParams } from 'react-router-dom';
 import NavBar from '../components/NavBar/NavBar';
@@ -19,15 +19,12 @@ function ListDetail() {
   // const [loading, error, data] = useDataFetching(
   //   'https://my-json-server.typicode.com/PacktPublishing/React-Projects-Second-Edition/items/',
   // );
-  const { loading, error, items: data } = useContext(ItemsContext);
-
-  const [items, setItems] = useState([]);
+  // const { loading, error, items: data } = useContext(ItemsContext);
+  const { loading, error, items, fetchItems } = useContext(ItemsContext);
 
   useEffect(() => {
-    data &&
-      listId &&
-      setItems(data.filter((item) => item.listId === parseInt(listId)));
-  }, [data, listId]);
+    listId && !items.length && fetchItems(listId);
+  }, [fetchItems, items, listId]);
 
   return (
     <>
